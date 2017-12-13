c7a94a465b988a824f2a3c1c79d85f3a7da65caf
kz <784248377@qq.com>
2017-12-13 15:32:56 +0800

    增加y2log

--- console/config/params.php
+++ console/config/params.php
@@ -1,4 +1,7 @@
 <?php
 return [
-    'adminEmail' => 'kitral.zhong@trainor.cn',
+	'adminEmail' => 'kitral.zhong@trainor.cn',
+	'y2log_alias' => [
+		'@rsfr' => '/home/master/pro/php/hsehome2.0/roadsafety/frontend',
+	]
 ];
