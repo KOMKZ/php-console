67bb791b5a99ca9ceab061743afa9f352f1b703c
kz <784248377@qq.com>
2018-01-22 20:34:01 +0800

    feat: 修改file

--- console/controllers/FileController.php
+++ console/controllers/FileController.php
@@ -3,6 +3,8 @@ namespace console\controllers;
 
 use Yii;
 use yii\console\Controller;
+use yii\helpers\VarDumper;
+use yii\helpers\ArrayHelper;
 
 /**
  *
@@ -19,16 +21,17 @@ class FileController extends Controller{
         $trContent = file_get_contents($trFile);
         $tableContent = file_get_contents($tableFile);
         $data = spyc_load_file($yamlFile);
-
         $content = "";
         foreach($data as $item){
             $trs = "";
             foreach($item['items'] as $def){
+                // active|可用|无描述|无标志|无默认
+                $def = $this->tr($def);
                 $trs .= strtr($trContent, [
                     '_val_' => $def['value'],
                     '_symbol_' => $def['symbol'],
                     '_name_' => $def['name'],
-                    '_des_' => $def['des'] ? implode("<br/>", $def['des']) : "",
+                    '_des_' => $def['des'] ? $def['des'] : "",
                     '_isdefault_' => (array_key_exists('isdefault', $def) && $def['isdefault']) ? "是" : ""
                 ]);
             }
@@ -36,5 +39,30 @@ class FileController extends Controller{
         }
         $html = strtr($htmlContent, ['__content__' => $content]);
         file_put_contents($outFile, $html);
+    }
+
+    protected function tr($item){
+        $def = explode('|', $item);
+        $def['value'] = $def[0];
+        $def['name'] = $def[1];
+        $def['des'] = ArrayHelper::getValue($def, 2, '');
+        $def['symbol'] = ArrayHelper::getValue($def, 3, '');
+        $def['isdefault'] = ArrayHelper::getValue($def, 4, '');
+        return $def;
+    }
+    public function actionEnumsStr($yamlFile, $file){
+        $data = spyc_load_file($yamlFile);
+        $result = [];
+        foreach($data as $item){
+            $enums = [];
+            foreach($item['items'] as $value){
+                $value = $this->tr($value);
+                $enums[] = sprintf("%s:%s", $value['value'], $value['name']);
+            }
+            $result[$item['field']] = implode('|', $enums);
+        }
+        $content = sprintf("<?php\nreturn %s;", VarDumper::export($result));
+        file_put_contents($file, $content);
+        echo $file;
     }
 }
