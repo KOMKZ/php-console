9f869d81565bbf20c3174c1929014d18b26ad7f4
kitral <784248377@qq.com>
2019-02-07 19:53:55 +0800

    %0%

--- console/config/params.php
+++ console/config/params.php
@@ -4,14 +4,31 @@ $desFormat = [Console::FG_RED];
 return [
     'cmds' => [
         'save_self' => [
-            sprintf("echo '%s'", Console::ansiFormat("保存php-console到github", $desFormat)),
+            sprintf("echo '%s'", Console::ansiFormat("保存php-console到github:%name% {commit}", $desFormat)),
             "cd %cmd_path%",
             "git add --all",
             'git commit -m "%0%"',
             'git push origin master'
         ],
+        'pwd' => [
+            sprintf("echo '%s'", Console::ansiFormat("查看kz密码:%name%", $desFormat))
+            ,'cat %kz_pwd_path%'
+            ,"echo \n"
+        ],
+        '5to7' => [
+            sprintf("echo '%s'", Console::ansiFormat("php5.6->php7.2:%name%", $desFormat))
+            ,'update-alternatives --set php /usr/bin/php5.6'
+            ,'service php5.6-fpm restart'
+            ,'service nginx restart'
+        ],
+        '7to5' => [
+            sprintf("echo '%s'", Console::ansiFormat("php7.2->php5.6:%name%", $desFormat))
+            ,'update-alternatives --set php /usr/bin/php7.2'
+            ,'service php7.2-fpm restart'
+            ,'service nginx restart'
+        ],
         'save_doc' => [
-            sprintf("echo '%s'", Console::ansiFormat("保存doc到私有仓库", $desFormat)),
+            sprintf("echo '%s'", Console::ansiFormat("保存doc到私有仓库:%name% {commit}", $desFormat)),
             "cd %doc_path%",
             "git add --all",
             'git commit -m "%0%"',
@@ -21,10 +38,68 @@ return [
             sprintf("echo '%s'", Console::ansiFormat("启动navicat", $desFormat))
             ,'cd %navicat_path%'
             ,'./start_navicat'
-
+        ],
+        'new_php5_serv' => [
+            sprintf("echo '%s'", Console::ansiFormat("新建php5.6-http服务:%name% {name} {port} {root}", $desFormat)),
+            'echo "%nginx_5.6_tpl%" > /etc/nginx/sites-enabled/%name%.conf',
+            'nginx -t',
+            'service nginx restart'
+        ],
+        'new_php7_serv' => [
+            sprintf("echo '%s'", Console::ansiFormat("新建php7.2-http服务:%name% {name} {port} {root}", $desFormat)),
+            'echo "%nginx_7.2_tpl%" > /etc/nginx/sites-enabled/%0%.conf',
+            'nginx -t',
+            'service nginx restart'
+        ],
+        'edit_ng_conf' => [
+            sprintf("echo '%s'", Console::ansiFormat("编辑nginx配置文件:%name% {name} {port}", $desFormat))
+            ,'gedit /etc/nginx/sites-enabled/%0%.conf',
         ]
     ],
+    'nginx_5.6_tpl' => '
+server {
+    # for %0%
+	listen %1% default_server;
+	listen [::]:%1% default_server ipv6only=on;
+	root %2%;
+	index index.php index.html index.htm;
+	server_name localhost;
+	location / {
+		try_files $uri $uri/ =404;
+	}
+	location ~ ^(?!/(assets|index.php|favicon.ico|index-test.php))(.*) {
+        rewrite ^(?!/index.php)(/.*) /index.php$2 last;
+    }
+	location ~ \.php$ {
+		fastcgi_split_path_info ^(.+\.php)(/.+)$;
+		fastcgi_pass unix:/run/php/php5.6-fpm.sock;
+		fastcgi_index index.php;
+		include fastcgi.conf;
+	}
+}',
+    'nginx_7.2_tpl' => '
+server {
+    # for %0%
+	listen %1% default_server;
+	listen [::]:%1% default_server ipv6only=on;
+	root %2%;
+	index index.php index.html index.htm;
+	server_name localhost;
+	location / {
+		try_files $uri $uri/ =404;
+	}
+	location ~ ^(?!/(assets|index.php|favicon.ico|index-test.php))(.*) {
+                rewrite ^(?!/index.php)(/.*) /index.php$2 last;
+    }
+	location ~ \.php$ {
+		fastcgi_split_path_info ^(.+\.php)(/.+)$;
+		fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+		fastcgi_index index.php;
+		include fastcgi.conf;
+	}
+}',
     'doc_path' => '',
     'cmd_path' => '',
     'navicat_path' => '',
+    'kz_pwd_path' => '',
 ];
--- console/controllers/RController.php
+++ console/controllers/RController.php
@@ -15,20 +15,81 @@ class RController extends Controller{
     public function actionIndex($name){
         $cmd = Yii::$app->params['cmds'][$name];
         $cmd = implode(';', $cmd);
-        preg_match_all('/(%[a-zA-Z_\-0-9]+%)/', $cmd, $params);
+        preg_match_all('/(%[a-zA-Z_\-\.0-9]+%)/', $cmd, $params);
+        $vars = $this->getVars($params[1]);
+        $cmd = strtr($cmd, $vars);
+        system($cmd);
+    }
+    public function actionHelp(){
+        foreach(Yii::$app->params['cmds'] as $name => $def){
+            $des = strtr($def[0], ['%name%' => $name]);
+            echo sprintf("%-15s%s\n", $name, $des);
+        }
+    }
+    public function actionAlias(){
+        $binCansFile = Yii::getAlias('@app/runtime/kz_bin_cans.txt');
+        $binCans = [];
+        if(file_exists($binCansFile)){
+            $binCans = unserialize(file_get_contents($binCansFile));
+            $binCans = $binCans ? $binCans : [];
+        }else{
+            file_put_contents($binCansFile, '');
+        }
+        foreach($binCans as $binPath){
+            $name = basename($binPath);
+            if(strpos($name, 'kz_') === 0){
+                unlink($binPath);
+            }
+        }
+        file_put_contents($binCansFile, '');
+        $binCans = [];
+
+
+        $binTpl = <<<js
+#!/bin/bash
+cd %path;
+./yii %action $*;
+js;
+        $root = dirname(dirname(__DIR__));
+        $tos = [
+            'kz_help' => 'r/help',
+            'kz_alias' => 'r/alias'
+        ];
+        foreach(Yii::$app->params['cmds'] as $name => $def){
+            $binName = 'kz_' . $name;
+            $tos[$binName] = 'r ' . $name;
+        }
+
+        foreach($tos as $name => $action){
+            echo sprintf("%s %s\n", $name, $action);
+            $content = strtr($binTpl, [
+                '%path' => $root,
+                '%action' => $action
+            ]);
+            $binPath = '/usr/local/bin/' . $name;
+            $binCans[] = $binPath;
+            file_put_contents($binPath, $content);
+            chmod($binPath, 0777);
+        }
+        file_put_contents($binCansFile, serialize($binCans));
+        echo "ok\n";
+    }
+    protected function getVars($need = []){
         $to = [];
-        foreach(array_splice($_SERVER['argv'], 3) as $key => $val){
+        foreach(array_slice($_SERVER['argv'], 3) as $key => $val){
             $to["%{$key}%"] = $val;
         }
-        foreach ($params[1] as $val) {
+        $to['%name%'] = $_SERVER['argv'][2];
+        foreach ($need as $val) {
             $key = trim($val, '%');
             if(isset(Yii::$app->params[$key])){
                 $to[$val] = Yii::$app->params[$key];
+                preg_match_all('/(%[a-zA-Z_\-\.0-9]+%)/', $to[$val], $params);
+                $to[$val] = strtr($to[$val], $this->getVars($params[1]));
             }
         }
-        $cmd = strtr($cmd, $to);
-        system($cmd);
-
+        return $to;
     }
-
+    protected function renderString($content, $vars){
+    }
 }
