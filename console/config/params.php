991886ea043a85f0766381edd17853f24491e11f
zhong <kitral@globalegrow.com>
2019-02-11 17:53:06 +0800

    cn_to_en

--- console/config/params.php
+++ console/config/params.php
@@ -4,19 +4,19 @@ $desFormat = [Console::FG_RED];
 return [
     'cmds' => [
         'save_self' => [
-            sprintf("echo '%s'", Console::ansiFormat("保存php-console到github:%name% {commit}", $desFormat)),
+            sprintf("echo '%s'", Console::ansiFormat("save php-console to github:%name% {commit}", $desFormat)),
             "cd %cmd_path%",
             "git add --all",
             'git commit -m "%0%"',
             'git push origin master'
         ],
         'pull_self' => [
-            sprintf("echo '%s'", Console::ansiFormat("拉取php-console:%name%", $desFormat)),
+            sprintf("echo '%s'", Console::ansiFormat("pull php-console:%name%", $desFormat)),
             "cd %cmd_path%",
             'git pull origin master'
         ],
         'pwd' => [
-            sprintf("echo '%s'", Console::ansiFormat("查看kz密码:%name%", $desFormat))
+            sprintf("echo '%s'", Console::ansiFormat("cat kz pwd:%name%", $desFormat))
             ,'cat %kz_pwd_path%'
             ,"echo \n"
         ],
@@ -33,48 +33,48 @@ return [
             ,'service nginx restart'
         ],
         'save_doc' => [
-            sprintf("echo '%s'", Console::ansiFormat("保存doc到私有仓库:%name% {commit}", $desFormat)),
+            sprintf("echo '%s'", Console::ansiFormat("save doc in private hub:%name% {commit}", $desFormat)),
             "cd %doc_path%",
             "git add --all",
             'git commit -m "%0%"',
             'git push origin master'
         ],
         'cat_host' => [
-            sprintf("echo '%s'", Console::ansiFormat("查看host:%name%", $desFormat))
+            sprintf("echo '%s'", Console::ansiFormat("check host:%name%", $desFormat))
             ,'cat /etc/hosts'
         ],
         'pull_doc' => [
-            sprintf("echo '%s'", Console::ansiFormat("拉取doc:%name% {commit}", $desFormat)),
+            sprintf("echo '%s'", Console::ansiFormat("pull doc:%name% {commit}", $desFormat)),
             "cd %doc_path%",
             'git pull origin master'
         ],
         'navicat' => [
-            sprintf("echo '%s'", Console::ansiFormat("启动navicat", $desFormat))
+            sprintf("echo '%s'", Console::ansiFormat("start navicat", $desFormat))
             ,'cd %navicat_path%'
             ,'./start_navicat'
         ],
         'new_php5_serv' => [
-            sprintf("echo '%s'", Console::ansiFormat("新建php5.6-http服务:%name% {name} {port} {root}", $desFormat)),
+            sprintf("echo '%s'", Console::ansiFormat("new php5.6-http server:%name% {name} {port} {root}", $desFormat)),
             'echo "%nginx_5.6_tpl%" > /etc/nginx/sites-enabled/%name%.conf',
             'nginx -t',
             'service nginx restart'
         ],
         'new_php7_serv' => [
-            sprintf("echo '%s'", Console::ansiFormat("新建php7.2-http服务:%name% {name} {port} {root}", $desFormat)),
+            sprintf("echo '%s'", Console::ansiFormat("new php7.2-http server:%name% {name} {port} {root}", $desFormat)),
             'echo "%nginx_7.2_tpl%" > /etc/nginx/sites-enabled/%0%.conf',
             'nginx -t',
             'service nginx restart'
         ],
         'proxy' => [
-            sprintf("echo '%s'", Console::ansiFormat("启动代理:%name%", $desFormat))
+            sprintf("echo '%s'", Console::ansiFormat("start proxy:%name%", $desFormat))
             ,'sslocal -c %proxy_conf_path%'
         ],
         'edit_ng_conf' => [
-            sprintf("echo '%s'", Console::ansiFormat("编辑nginx配置文件:%name% {name} {port}", $desFormat))
+            sprintf("echo '%s'", Console::ansiFormat("edit nginx conf file:%name% {name} {port}", $desFormat))
             ,'gedit /etc/nginx/sites-enabled/%0%.conf',
         ],
         'ngrs' => [
-            sprintf("echo '%s'", Console::ansiFormat("重启nginx:%name%", $desFormat))
+            sprintf("echo '%s'", Console::ansiFormat("restart nginx:%name%", $desFormat))
             ,'service nginx restart'
         ]
     ],
